fc7aa75061973438294a29083ab239e2ff143b2f
demetrios <demetrios.reis@onebox.one>
2024-05-06 17:14:02 -0300

    init project and init modular

---
+++ lib/app_widget.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) => const AppWidget();
+}
